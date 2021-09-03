8c0d7ea207a3f0ea472e79355fa6687517958076
Hritik Shelar <1hritik2shelar45@gmail.com>
2021-09-03 20:44:54 +0530

    Metro Medical v1.0

--- app.js
+++ app.js
@@ -125,6 +125,12 @@ app.use('/cart', cart);
 app.use('/users',users)
 app.use('/', pages);
 
-app.listen(3000,() => {
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port=3000;
+}
+
+
+app.listen(port,() => {
   console.log("Server started on port 3000");
 });
