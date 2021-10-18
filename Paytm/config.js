1d92ec8fb38d3468971edb9f9930214205b7da6e
Hritik Shelar <1hritik2shelar45@gmail.com>
2021-10-18 15:23:09 +0530

    Metromedical Panvel 2021

--- Paytm/config.js
+++
@@ -1,6 +0,0 @@
-var PaytmConfig = {
-  mid: "Kkatvd34334425459416",
-  key: "fgkmv_RHsi6R@QXm",
-  website: "WEBSTAGING",
-};
-module.exports.PaytmConfig = PaytmConfig;
--- routes/cart.js
+++ routes/cart.js
@@ -4,8 +4,8 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const parseUrl = express.urlencoded({extended:true});
 const parseJson = express.json({extended:true})
-const checksum_lib = require('../Paytm/checksum');
-const config = require('../Paytm/config');
+const stripe = require('stripe')("sk_test_51JlSJVSDpQSnIWsEPg9moY8Frxsc0xW1ccfPOwqa8cQDRcMTwMHmYlUs72uIhUR0h3NQZyjqznh70TOyvTWfzsnN00VCGd0F8D");
+
 
 router.get('/add/:product', (req,res) => {
 
@@ -54,7 +54,7 @@ router.get('/checkout', (req,res) => {
     delete req.session.cart;
     res.redirect('/cart/checkout');
   } else {
-    res.render('checkout', {title: "Checkout", cart: req.session.cart});
+    res.render('checkout', {title: "Checkout", cart: req.session.cart, key:"pk_test_51JlSJVSDpQSnIWsEKqynYzvHn4zAbSrDkEKDFulbtnHqFPZzx8Kqfg4HrA9VOAw78f80mDYZUjMrmDPNMGX7KWlk00RJVaeTXa"});
   }
 
 })
@@ -90,7 +90,7 @@ router.get('/clear', (req,res) => {
   res.redirect('/cart/checkout');
 })
 
-router.get('/buynow', (req,res) => {
+router.get('/payment', (req,res) => {
 
   res.sendStatus(200);
 
@@ -105,116 +105,34 @@ router.get('/buynow', (req,res) => {
   delete req.session.cart;
 })
 
-router.post("/paynow", [parseUrl, parseJson], (req, res) => {
-  // Route for making payment
-  let paymentDetails = {
-    amount: req.body.amount,
-    customerId: res.locals.user.username,
-    customerEmail: res.locals.user.email
-  }
+router.post('/payment', (req,res) => {
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken,
+    name:'Hritik Shelar',
+    address: {
+      line1: 'Metro Medical Panvel',
+      postal_code: '410106',
+      city:'New Mumbai',
+      state:'Maharashtra',
+      country:'India'
+    }
+  })
+  .then((customer) => {
+    return stripe.charges.create({
+      amount: req.body.amount,
+      description: 'Metromedical Panvel',
+      currency: 'INR',
+      customer: customer.id
+    })
+  })
+  .then((charge) => {
+    res.send("Success");
+  })
+  .catch(err => {
+    res.send(err);
+  })
+})
 
-if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail) {
-    res.status(400).send('Payment failed')
-} else {
-    var params = {};
-    params['MID'] = config.PaytmConfig.mid;
-    params['WEBSITE'] = config.PaytmConfig.website;
-    params['CHANNEL_ID'] = 'WEB';
-    params['INDUSTRY_TYPE_ID'] = 'Retail';
-    params['ORDER_ID'] = 'TEST_'  + new Date().getTime();
-    params['CUST_ID'] = paymentDetails.customerId;
-    params['TXN_AMOUNT'] = paymentDetails.amount;
-    params['CALLBACK_URL'] = 'http://localhost:3000/callback';
-    params['EMAIL'] = paymentDetails.customerEmail;
-    params['MOBILE_NO'] = paymentDetails.customerPhone;
-
-
-    checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
-        var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
-        // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
-
-        var form_fields = "";
-        for (var x in params) {
-            form_fields += "<input type='hidden' name='" + x + "' value='" + params[x] + "' >";
-        }
-        form_fields += "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "' >";
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' + txn_url + '" name="f1">' + form_fields + '</form><script type="text/javascript">document.f1.submit();</script></body></html>');
-        res.end();
-    });
-}
-});
-
-router.post("/callback", (req, res) => {
-  // Route for verifiying payment
-
-  var body = '';
-
-  req.on('data', function (data) {
-     body += data;
-  });
-
-   req.on('end', function () {
-     var html = "";
-     var post_data = qs.parse(body);
-
-     // received params in callback
-     console.log('Callback Response: ', post_data, "\n");
-
-
-     // verify the checksum
-     var checksumhash = post_data.CHECKSUMHASH;
-     // delete post_data.CHECKSUMHASH;
-     var result = checksum_lib.verifychecksum(post_data, config.PaytmConfig.key, checksumhash);
-     console.log("Checksum Result => ", result, "\n");
-
-
-     // Send Server-to-Server request to verify Order Status
-     var params = {"MID": config.PaytmConfig.mid, "ORDERID": post_data.ORDERID};
-
-     checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
-
-       params.CHECKSUMHASH = checksum;
-       post_data = 'JsonData='+JSON.stringify(params);
-
-       var options = {
-         hostname: 'securegw-stage.paytm.in', // for staging
-         // hostname: 'securegw.paytm.in', // for production
-         port: 443,
-         path: '/merchant-status/getTxnStatus',
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/x-www-form-urlencoded',
-           'Content-Length': post_data.length
-         }
-       };
-
-
-       // Set up the request
-       var response = "";
-       var post_req = https.request(options, function(post_res) {
-         post_res.on('data', function (chunk) {
-           response += chunk;
-         });
-
-         post_res.on('end', function(){
-           console.log('S2S Response: ', response, "\n");
-
-           var _result = JSON.parse(response);
-             if(_result.STATUS == 'TXN_SUCCESS') {
-                 res.send('payment sucess')
-             }else {
-                 res.send('payment failed')
-             }
-           });
-       });
-
-       // post the data
-       post_req.write(post_data);
-       post_req.end();
-      });
-     });
-});
 
 module.exports = router;
